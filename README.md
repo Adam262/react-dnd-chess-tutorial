221e1928d6e1b4eb985212447d2c80083f3ce86f
Adam Barcan <abarcan@gmail.com>
2018-01-28 15:17:55 -0500

    Add canDrop() validation on board_square - the drop target

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ This is my work for the official [react-dnd tutorial](http://react-dnd.github.io
 
 It consists of a chess board with a single knight piece. You can drag and drop the knight per the rules of chess with validations.
 
-The code is highly annotated to reflect my understanding of the key library components. If interested take a look at:
+My code is highly annotated to reflect my understanding of the key library components, as of the `react-dnd` version set in `yarn.lock`. 
+
+If interested take a look at:
 
 * src/chess/board_square.jsx (the [DropTarget](http://react-dnd.github.io/react-dnd/docs-drop-target.html));
 * src/chess/knight.jsx (the [DragSource](http://react-dnd.github.io/react-dnd/docs-drag-source.html))
@@ -14,7 +16,7 @@ The code is highly annotated to reflect my understanding of the key library comp
 `git clone git@github.com:Adam262/react-dnd-chess-tutorial.git`
 `cd` into project directory
 `yarn install`
-`gastby develop`
+`gatsby develop`
 
 You can see local site at `localhost:8000`
 
--- src/chess/board_square.jsx
+++ src/chess/board_square.jsx
@@ -12,15 +12,28 @@ import Square from './square';
   - options
 */
 import { DropTarget } from 'react-dnd';
-import { ItemTypes } from './constants';
+import { ItemTypes, colors } from './constants';
 
 import styled from 'styled-components';
 
-// This is ES6 object syntax, equivalent to:
-// var squareTarget = { drop: function(props) { do stuff } }
+/*
+  This is the spec object. 
+
+  We've written it in ES6 object syntax, equivalent to:
+    var squareTarget = { drop: function(props) { do stuff } }
+
+  It can have methods:
+  - canDrop(props, monitor, component)
+  - drop(props, monitor, component)
+  - hover(props, monitor)
+*/
 const squareTarget = {
+  canDrop(props) {
+    return canMoveKnight(props.x, props.y);
+  },
+
   drop(props) {
-    canMoveKnight(props.x, props.y) && moveKnight(props.x, props.y);
+    moveKnight(props.x, props.y);
   }
 };
 
@@ -29,15 +42,16 @@ function collect(connect, monitor) {
     /*
       http://react-dnd.github.io/react-dnd/docs-drop-target-connector.html
 
-      DropTargetConnector is an object passed to the collecting function of the DropTarget. 
-      Its only method dropTarget() returns a function that lets you assign the drop target role to one of your component's DOM nodes.
+      collect(connect, monitor) returns an object that lets you pass methods from its arguments to your component as props: 
 
-      Call the DropTargetConnector's dropTarget() method inside your collecting function. 
-      This will pass the returned function to your component as a prop.
-      You can then use it inside render or componentDidMount to indicate a DOM node should react to drop target events
+        - connect is an instance of DropTargetConnector, an object with the single method connect.dropTarget(). 
+          connect.dropTarget() is called in render(); it wraps an element to make it a drop target for draggable elements.
+        - monitor is an instance of DropTargetMonitor. it is used to query info on the drag state
+
     */
     connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
+    monitorCanDrop: monitor.canDrop(),
+    monitorIsOver: monitor.isOver(),
   };
 }
 
@@ -45,13 +59,15 @@ class BoardSquare extends React.Component {
   static propTypes = {
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired,
-    // The collect() function injects connectDropTarget and isOver into props
+
+    // The collect() function injects the object returned by collect() into your component props
     connectDropTarget: PropTypes.func.isRequired,
-    isOver: PropTypes.bool.isRequired
+    monitorCanDrop: PropTypes.bool.isRequired,
+    monitorIsOver: PropTypes.bool.isRequired,
   };
 
   render() {
-    const { x, y, children, connectDropTarget, isOver } = this.props;
+    const { x, y, children, connectDropTarget, monitorCanDrop, monitorIsOver } = this.props;
     const black = (x + y) % 2 === 1;
   
     return connectDropTarget(
@@ -61,7 +77,7 @@ class BoardSquare extends React.Component {
         height: '100%'
       }}>
         <Square black={black}> {children} </Square>
-        {isOver && <HighlightOverlay validMove={canMoveKnight(x, y)} />}
+        { monitorIsOver && <HighlightOverlay canDrop={ monitorCanDrop } /> }
       </div>
     );
   }
@@ -75,7 +91,7 @@ const HighlightOverlay = styled.div`
   width: 100%;
   zIndex: 1;
   opacity: 0.5;
-  background: ${props => !!props.validMove ? '#66FF33' : 'red'};
+  background: ${ props => props.canDrop ? colors.lightGreen : colors.red };
 `;
 
 // Every DropTarget needs at least one corresponding DragSource
